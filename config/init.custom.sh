4e1ae4e812e9c2c4180e1a089fde405597c59a85
Julian Pustkuchen <Julian@Pustkuchen.com>
2020-12-21 15:12:09 +0100

    Fixes

--- config/init.custom.sh
+++ config/init.custom.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-cd /app/web
+cd /app
 
 # Custom drupal installation
 ## Project specific - put into separate file!
 echo 'Installing project specific dependencies:'
 # Enable bartik default theme
-lando drush theme:enable -y bartik
-lando drush config:set -y system.theme default bartik
+drush theme:enable -y --root=/app/web bartik --root=/app/web
+drush config:set -y --root=/app/web system.theme default bartik
 
 # Enable claro admin theme
-lando drush theme:enable -y claro
-lando drush config:set -y system.theme admin claro
+drush theme:enable -y --root=/app/web claro
+drush config:set -y --root=/app/web system.theme admin claro
 
 # Install required core modules:
-lando drush en -y automated_cron block ckeditor config contextual block_custom menu_link_content dblog field field_ui filter menu_ui node path quickedit taxonomy editor update user views views_ui text options link image file datetime
+drush en -y --root=/app/web automated_cron block ckeditor config contextual block_custom menu_link_content dblog field field_ui filter menu_ui node path quickedit taxonomy editor update user views views_ui text options link image file datetime
 
 # Install contrib modules:
-lando composer require -W drupal/admin_toolbar drupal/devel drupal/devel_php drupal/devel_debug_log drupal/coder drupal/examples drupal/stage_file_proxy
+composer require -W drupal/admin_toolbar drupal/devel drupal/devel_php drupal/devel_debug_log drupal/coder drupal/examples drupal/stage_file_proxy
 # Enable contrib modules:
-lando drush en -y admin_toolbar admin_toolbar_tools admin_toolbar_search devel devel_php devel_debug_log examples stage_file_proxy hacked
+drush en -y --root=/app/web admin_toolbar admin_toolbar_tools admin_toolbar_search devel devel_php devel_debug_log examples stage_file_proxy hacked
 
 git add .
 git diff-index --quiet HEAD || git commit -am "After init.custom.sh"
