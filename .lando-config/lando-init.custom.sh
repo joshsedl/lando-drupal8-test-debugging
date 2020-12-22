dab7a4dae8f3c263f63462b17cead995fed234ea
Julian Pustkuchen <Julian@Pustkuchen.com>
2020-12-22 05:09:41 +0100

    Added --ansi

--- .lando-config/lando-init.custom.sh
+++ .lando-config/lando-init.custom.sh
@@ -22,7 +22,7 @@ drush en -y --root=/app/web automated_cron block block_content ckeditor config c
 
 # Install contrib modules:
 echo "-- Require and enable several development modules: --"
-composer require --dev drupal/admin_toolbar drupal/devel drupal/devel_php drupal/devel_debug_log drupal/coder drupal/examples drupal/stage_file_proxy
+composer require --dev --ansi drupal/admin_toolbar drupal/devel drupal/devel_php drupal/devel_debug_log drupal/coder drupal/examples drupal/stage_file_proxy
 drush en -y --root=/app/web admin_toolbar admin_toolbar_tools admin_toolbar_search devel devel_generate devel_php devel_debug_log stage_file_proxy webprofiler
 
 echo "-- Final lando-init.custom.sh git add & commit: --"
